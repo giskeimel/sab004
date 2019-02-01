c793c5ea084ba48ba7a7e5d9668d14bd18a104c0
Sergio Ariel Pan <sergiomail555@gmail.com>
2019-02-01 08:06:43 -0300

    Se agrego ConsultaLugar  y el paquete de excepciones, se modifico la clase de mapeo de lugar
    se agrego ConsultaLugar al paquete com.mycompany.tusalud.db y se agrego el paquete de excepciones con las clases TuSaludExcepcion, BDExcepcion y  se modifico el mapeo de la clase lugar

---
+++ TuSalud/TUSALUD/src/main/java/com/mycompany/tusalud/db/ConsultaLugar.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tusalud.db;
+
+import HibernateClase.HibernateUtilities;
+import com.mycompany.tusalud.data.Lugar;
+import com.mycompany.tusalud.excepciones.BDException;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author User
+ */
+public class ConsultaLugar {
+    
+    public ConsultaLugar(){}
+    
+    public Lugar getLugarPorNombre(String nombre) throws BDException{
+        try{
+            Session session = HibernateUtilities.getSession();
+            session.beginTransaction();
+            
+            Lugar lugar = (Lugar) session.createQuery("SELECT l FROM Lugar l WHERE l.nombre LIKE " + nombre);
+            
+            session.getTransaction().commit();
+            session.close();
+            
+            if (lugar == null){
+                throw new BDException("No se encontró");
+            }
+            
+            return lugar;
+        }catch (HibernateException e) {
+            e.printStackTrace();
+            throw new BDException("Error al pedir la lista de turnos", e);
+        }
+    }
+    
+    public Lugar getLugar(Integer idLugar) throws BDException{
+        try{
+            Session session = HibernateUtilities.getSession();
+            session.beginTransaction();
+            
+            Lugar lugar = (Lugar) session.get(Lugar.class, idLugar);          
+            
+            session.getTransaction().commit();
+            session.close();
+            
+            if (lugar == null){
+                throw new BDException("No se encontró");
+            }
+            
+            return lugar;
+        }catch (HibernateException e) {
+            e.printStackTrace();
+            throw new BDException("Error al pedir la lista de turnos", e);
+        }
+    }
+    
+    public void guardarLugar(Lugar lugar) throws BDException {
+        try {
+            Session session = HibernateUtilities.getSession();
+            session.beginTransaction();
+            
+            session.saveOrUpdate(lugar);
+            
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            throw new BDException("Error al guardar el lugar", e);
+        }
+    }
+    
+    public List getListaEspecialidades(Lugar lugar) throws BDException{
+        
+        if (! lugar.getEspecialidades().isEmpty()){
+            return lugar.getEspecialidades();
+        }else{
+            throw new BDException("Error al pedir la lista de especialidades");
+        }
+    }
+    
+    public List getListaTurnos(Lugar lugar) throws BDException{
+        
+        if (! lugar.getTurnos().isEmpty()){
+            return lugar.getTurnos();
+        }else{
+            throw new BDException("Error al pedir la lista de turnos");
+        }
+    }
+}
